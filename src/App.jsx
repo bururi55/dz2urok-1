729810a132266b33467b49405b2f3123300df041
MaksimBururi <m.lubitkuy@gmail.com>
2025-01-26 12:06:04 +0300

    peavki1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import ReactLogo from './assets/react.svg?react';
 import { MyComponent } from './Mycomponent';
-import styles from './index.module.css';
 
 export const App = () => {
 	return (
-		<div className={styles.app}>
-			<h1 className={styles.appHeader}>Hello World</h1>
+		<div>
+			<h1>Hello World</h1>
 			<ReactLogo />
 			<MyComponent />
 		</div>
--- src/Mycomponent.jsx
+++ src/Mycomponent.jsx
@@ -1,17 +1,2 @@
 import { useState } from 'react';
 import styles from './MyComponent.module.css';
-
-export const MyComponent = () => {
-	const [showRedText, setShowRedText] = useState(false);
-
-	const onClick = () => {
-		setShowRedText(!showRedText);
-	};
-
-	return (
-		<>
-			<div className={showRedText ? styles.red : styles.white}>Текст</div>
-			<button onClick={onClick}>Изменить цвет текста</button>
-		</>
-	);
-};
